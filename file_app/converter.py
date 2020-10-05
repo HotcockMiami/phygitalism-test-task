1d21bc24d06579ecbacbc5693d8ac39f320b56dc
HotcockMiami <svd-50002@mail.ru>
2020-10-06 00:50:49 +0300

    Update converter.py

--- file_app/converter.py
+++ file_app/converter.py
@@ -5,7 +5,7 @@
 Прошу обратить внимание, что он не реализовывает
 рекурсивный поиск, судя по заданию я решил что в этом
 нет необходимости
-Даты обе, создания и изменения, так как это хорошая практика
+Даты обе, создания и изменения (по возможности и зависит от ОС)
 '''
 import os
 import platform # Модуль для определения ОС
@@ -19,27 +19,26 @@ def dir_to_json(dirname: str) -> dict: # Обозначаем директори
         full_path = os.path.join(dirname, name)
         file_type = os.path.splitext(full_path)[1]
         dct['name'] = os.path.splitext(os.path.basename(full_path))[0]
-        # Если файл - не папка, то задаём ему соответствующий тип
-        if os.path.isfile(full_path):
+
+        if os.path.isfile(full_path): # Если файл - не папка, то задаём ему соответствующий тип
             dct['type'] = file_type
-        # Если папка, то так и записываем
-        elif os.path.isdir(full_path):
+        elif os.path.isdir(full_path): # Если папка, то так и записываем
             dct['type'] = 'folder'
-        #Начинаем смотреть и добавлять данные по дате файла
+
+        # Начинаем смотреть и добавлять данные по дате файла
         if platform.system() == 'Windows': 
             # Если сервер запущен на Виндоус, то получить дату создания файла достаточно просто
             dct['created_at']= time.ctime(os.path.getctime(full_path))
             dct['modified_at'] = time.ctime(os.path.getmtime(full_path))
         else:
-            #Для Mac и Linux
-            stat = os.stat(full_path)
+            stat = os.stat(full_path) # Для Mac и Linux
             try:
                 dct['created_at'] = time.ctime(stat.st_birthtime)
             except AttributeError:
                 # Утыкаемся в Линукс, получить время создания файла для него адекватным способом
-                # Не представля возможным, так что используем только время изменения
+                # Не представляется возможным, так что используем только время изменения
                 dct['modified_at'] = time.ctime(stat.st_mtime)
-        json['data'].append(dct)
+        json['data'].append(dct) # Составляем итоговый json и возвращаем его
     return json
 
 if __name__ == "__converter__":
